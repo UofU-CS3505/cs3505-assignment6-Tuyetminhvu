99ee2f5fb1e37a8f2f5055711d04a6baa6c729bb
fernandolemus051 <fernandolemus051@gmail.com>
2026-03-05 14:30:50 -0700

    implemented logic methods for simongame

--- SimonGame/simongame.cpp
+++ SimonGame/simongame.cpp
@@ -3,3 +3,45 @@
 SimonGame::SimonGame(QObject *parent)
     : QObject{parent}
 {}
+
+void SimonGame::startGame(){
+    buttonSequence.append(getRandomNumber());
+    playSequence(0);
+}
+
+void SimonGame::button1Pressed(){
+    if (buttonSequence[sequenceIndex] == 1){
+        buttonSequence.append(getRandomNumber());
+        playSequence(0);
+    } else {
+        // Game over
+        buttonSequence.clear();
+    }
+}
+
+void SimonGame::button2Pressed(){
+    if (buttonSequence[sequenceIndex] == 2){
+        buttonSequence.append(getRandomNumber());
+        playSequence(0);
+    } else {
+        // Game over
+        buttonSequence.clear();
+    }
+}
+
+int SimonGame::getRandomNumber(){
+    return QRandomGenerator::global()->bounded(1, 3);
+}
+
+void SimonGame::playSequence(int index)
+{
+    // Base case, return when last index is reached
+    if (index >= buttonSequence.size()) return;
+
+    if (buttonSequence[index] == 1)
+        emit flashButton1();
+    else
+        emit flashButton2();
+
+    QTimer::singleShot(500, this, [=]() {playSequence(index + 1);});
+}
--- SimonGame/simongame.h
+++ SimonGame/simongame.h
@@ -2,6 +2,8 @@
 #define SIMONGAME_H
 
 #include <QObject>
+#include <QRandomGenerator>
+#include <QTimer>
 
 class SimonGame : public QObject
 {
@@ -10,15 +12,59 @@ public:
     explicit SimonGame(QObject *parent = nullptr);
 
 public slots:
+    /**
+     * @brief startGame
+     * Initiates the game.
+     * Sets the first number in the sequence.
+     */
+    void startGame();
+
+    /**
+     * @brief button1Pressed
+     * A slot for the first button to verify
+     * if is the correct number in the sequence.
+     */
     void button1Pressed();
-    void button2Presssed();
+
+    /**
+     * @brief button2Pressed
+     * A slot for the second button to verify
+     * if is the correct number in the sequence.
+     */
+    void button2Pressed();
 
 signals:
     void flashButton1();
     void flashButton2();
 
 private:
+    /**
+     * @brief buttonSequence
+     * vector to hold the sequence of numbers (1 or 2)
+     */
     QVector<int> buttonSequence;
+
+    /**
+     * @brief sequenceIndex
+     * Keeps track of what index the buttonSequence is
+     */
+    int sequenceIndex = 0;
+
+    /**
+     * @brief getRandomNumber
+     * A helper method to get a random
+     * number (1 or 2) to add to the number sequence.
+     * @return a random number 1 or 2
+     */
+    int getRandomNumber();
+
+    /**
+     * @brief playSequence
+     * recursively iterates through the buttonSequence and emits signals to the view
+     * depending on what number is in the sequence.
+     * @param index Starts at the index passed by parameter
+     */
+    void playSequence(int index);
 
 };
 
