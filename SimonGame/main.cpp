1239ea7214ef9b84215cba1c75b07fb52459731f
fernandolemus051 <fernandolemus051@gmail.com>
2026-03-11 21:17:14 -0600

    Added top-of-file comments to main.cpp

--- SimonGame/main.cpp
+++ SimonGame/main.cpp
@@ -1,5 +1,15 @@
 /**
- * The entry point for the Simon Game application.
+ * Teammembers: Fernando Lemus Guilen & Tuyet Minh Vu
+ * Github usernames: fernandolemus051 & Tuyetminhvu
+ * Github repo URL: github.com/UofU-CS3505/cs3505-assignment6-Tuyetminhvu
+ * Creative elements:
+ * 1. Although not able to hear in the videw recording, we added background music into the game
+ * 2. Fun GIFs to let a player know when its their turn and when they lose.
+ * 3. Background image to make it feel 'polished' and engaging.
+ */
+
+/**
+ * The entry point for the Simon Game app.
  * Initializes the Qt Application, the Game Model, and the Main Window.
  * @author Fernando Lemus Guillen & Tuyet Minh Vu
  * @date 2026-03-10
